323b4d6eff7af00d2a130f046ac95f19f6b87136
Makita777 <kmakita@dcs.co.jp>
2022-08-26 15:28:35 +0900

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,25 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductChildComponent } from './product-child/product-child.component';
+import { Names } from './names';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
+    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductChildComponent,
+    Names,
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-
+export class AppModule {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
---
+++ src/app/names.ts
@@ -0,0 +1,27 @@
+export const Names = [
+  {
+    name: 'Aさん',
+    compNo: 1,
+    groupNo: '1Gr',
+  },
+  {
+    name: 'Bさん',
+    compNo: 2,
+    groupNo: '2Gr',
+  },
+  {
+    name: 'Cさん',
+    compNo: 3,
+    groupNo: '3Gr',
+  },
+  {
+    name: 'Child Aさん',
+    compNo: 4,
+    groupNo: '1Gr',
+  },
+  {
+    name: 'Child Bさん',
+    compNo: 5,
+    groupNo: '2Gr',
+  },
+];
